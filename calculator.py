7cac4bc2bdac62c333348d55603603e6fd2146ec
sharanyareddydonapati-hub <sharanyareddydonapati@gmail.com>
2026-05-21 20:14:50 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,19 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+operation = input("Enter operation (+,-,*,/): ")
+
+if operation == "+":
+    print(num1 + num2)
+
+elif operation == "-":
+    print(num1 - num2)
+
+elif operation == "*":
+    print(num1 * num2)
+
+elif operation == "/":
+    print(num1 / num2)
+
+else:
+    print("Invalid operation")
