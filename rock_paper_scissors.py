1643fbc310ba28a8c6594a21eb1536d1c43f1eb8
sharanyareddydonapati-hub <sharanyareddydonapati@gmail.com>
2026-05-21 20:27:52 +0530

    Add files via upload

---
+++ rock_paper_scissors.py
@@ -0,0 +1,23 @@
+import random
+
+choices = ["rock", "paper", "scissors"]
+
+user = input("Enter rock, paper, or scissors: ").lower()
+
+computer = random.choice(choices)
+
+print("Computer chose:", computer)
+
+if user == computer:
+    print("It's a tie!")
+
+elif (user == "rock" and computer == "scissors") or \
+     (user == "paper" and computer == "rock") or \
+     (user == "scissors" and computer == "paper"):
+    print("You win!")
+
+elif user in choices:
+    print("You lose!")
+
+else:
+    print("Invalid input!")
